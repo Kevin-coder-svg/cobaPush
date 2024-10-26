9745306358f59184afbd3282e0b8abb8b97c0194
Kevin-coder-svg <c14240002.john.petra.ac.id>
2024-10-26 15:09:38 +0700

    Feat: membuat master.java

---
+++ master.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class master{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Nama : ");
+        String name = input.nextLine();
+        System.out.println("Welc " + name);
+        System.out.println("Edit");
+    }
+} 
